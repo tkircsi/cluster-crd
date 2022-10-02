4fed20d5e173c0f81bfef8cc0c96c5ec2389e6ab
Tibor Kircsi <tkircsi@gmail.com>
2022-10-02 17:03:07 +0200

    Build an informer

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/tkircsi/cluster-crd/api/types/v1alpha1"
 	clientV1alpha1 "github.com/tkircsi/cluster-crd/clientset/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
@@ -50,4 +55,32 @@ func main() {
 
 	fmt.Printf("clusters found: %+v\n", clusters)
 
+	store := WatchResources(clientSet)
+
+	for {
+		clustersFromStore := store.List()
+		fmt.Printf("clusters in store: %d\n", len(clustersFromStore))
+		time.Sleep(1 * time.Second)
+
+	}
+
+}
+
+func WatchResources(clientSet clientV1alpha1.ExtensionV1Alpha1Interface) cache.Store {
+	clusterStore, clusterController := cache.NewInformer(
+		&cache.ListWatch{
+			ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+				return clientSet.Clusters("").List(options)
+			},
+			WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+				return clientSet.Clusters("").Watch(options)
+			},
+		},
+		&v1alpha1.Cluster{},
+		30*time.Second,
+		cache.ResourceEventHandlerFuncs{},
+	)
+
+	go clusterController.Run(wait.NeverStop)
+	return clusterStore
 }
